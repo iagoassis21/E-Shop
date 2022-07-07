62846e65d91dab0df2619cbb8fed976498039661
Danilo Martins <dansm86@outlook.com>
2022-07-07 18:37:09 -0300

    Req3-Criado componente Cart

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ProductList extends Component {
   render() {
@@ -17,6 +18,9 @@ class ProductList extends Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <nav>
+          <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
+        </nav>
       </div>
     );
   }
