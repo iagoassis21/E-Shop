9d7395e09d3d23a5d50e7c1461c6c4a451224634
Matheus Bora <matheusmcb@gmail.com>
2022-07-08 18:27:42 -0300

    req7-fixado os routes

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,13 +6,84 @@ import Details from '../pages/Details';
 import ProductList from '../pages/ProductList';
 
 class Content extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      productItem: '',
+      listItems: '',
+    };
+  }
+
+  componentDidMount() {
+    this.showCategories();
+  }
+
+  handleSubmit(ev) {
+    ev.preventDefault();
+  }
+
+  handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  showCategories = async () => {
+    const result = await getCategories();
+    console.log(result);
+    this.setState({
+      categories: result,
+    });
+  }
+
+  searchProductByQuery = async () => {
+    const { productItem } = this.state;
+    const { results } = await getProductsFromCategoryAndQuery(null, productItem);
+    this.setState({
+      listItems: results,
+    });
+  }
+
+  searchProductsByCategoryId = async (id) => {
+    const { results } = await getProductsFromCategoryAndQuery(id, null);
+    console.log(results);
+    this.setState({
+      listItems: results,
+    });
+  }
+
   render() {
+    const {
+      categories,
+      productItem,
+      listItems } = this.state;
+
     return (
-      <Switch>
-        <Route exact path="/" component={ ProductList } />
-        <Route path="/cart" component={ Cart } />
-        <Route path="/details/:id" component={ Details } />
-      </Switch>
+      <div>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              <ProductList
+                categories={ categories }
+                productItem={ productItem }
+                listItems={ listItems }
+              />) }
+          />
+          <Route path="/cart" render={ () => (<Cart />) } />
+          <Route
+            path="/details/:id"
+            render={ (routeProps) => (
+              <Details
+                { ...routeProps }
+                listItems={ listItems }
+              />) }
+          />
+        </Switch>
+      </div>
     );
   }
 }
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -6,53 +6,7 @@ import Products from '../components/Products';
 // import Details from './Details';
 
 class ProductList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      categories: [],
-      productItem: '',
-      listItems: '',
-    };
-  }
-
-  componentDidMount() {
-    this.showCategories();
-  }
-
-  handleSubmit(ev) {
-    ev.preventDefault();
-  }
-
-  handleInputChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  showCategories = async () => {
-    const result = await getCategories();
-    console.log(result);
-    this.setState({
-      categories: result,
-    });
-  }
-
-  searchProductByQuery = async () => {
-    const { productItem } = this.state;
-    const { results } = await getProductsFromCategoryAndQuery(null, productItem);
-    this.setState({
-      listItems: results,
-    });
-  }
-
-  searchProductsByCategoryId = async (id) => {
-    const { results } = await getProductsFromCategoryAndQuery(id, null);
-    console.log(results);
-    this.setState({
-      listItems: results,
-    });
-  }
+ 
 
   render() {
     const { categories, productItem, listItems } = this.state;
