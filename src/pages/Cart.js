62e9573b1b5a61cba6f3123ee243d9300d60dc82
Danilo Martins <dansm86@outlook.com>
2022-07-11 11:19:37 -0300

    Re10-adic botões para alterar quantidades Cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class Cart extends Component {
   render() {
-    const { cartItems } = this.props;
+    const { cartItems, onChangeQuantity } = this.props;
 
     return (
       <div>
@@ -22,6 +22,20 @@ class Cart extends Component {
                 { item.quantity }
                 {' '}
               </p>
+              <button
+                data-testid="product-decrease-quantity"
+                type="button"
+                onClick={ () => { onChangeQuantity('rem', item.id, cartItems); } }
+              >
+                -
+              </button>
+              <button
+                data-testid="product-increase-quantity"
+                type="button"
+                onClick={ () => { onChangeQuantity('add', item.id, cartItems); } }
+              >
+                +
+              </button>
             </article>
           ))
         }
